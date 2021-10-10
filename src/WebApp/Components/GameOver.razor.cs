16314c686ff1e54fefd60815fe0e64fee6a61a4f
Erin McLaughlin <emclaughlin@lighthouseinstruments.com>
2021-10-10 11:29:48 -0400

    Add fake score saving mechanic

--- src/WebApp/Components/GameOver.razor.cs
+++ src/WebApp/Components/GameOver.razor.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Threading.Tasks;
+using WebApp.Models;
 using WebApp.Services;
 
 namespace WebApp.Components
 {
     public partial class GameOver
     {
-        
+        private EditContext? EditContext { get; set; }
+        private HighScoreModel? ScoreModel { get; set; }
 
+        private bool Saved { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (GameService.Playlist is null)
+                Navigation.NavigateTo("");
+
+            ScoreModel = new HighScoreModel(GameService.Playlist!.Id, GameService.Score, GameService.Tracks!.Count);
+            EditContext = new(ScoreModel);
+
+            base.OnInitialized();
+        }
+
+        private async Task OnValidSubmit()
+        {
+            Saved = true;
+        }
     }
 }
