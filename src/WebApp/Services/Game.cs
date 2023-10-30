36546e3dadad19ec8e855d8ff5544d5d081b870e
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-10-29 20:32:38 -0400

    more ui wip

---
+++ src/WebApp/Services/Game.cs
@@ -0,0 +1,15 @@
+﻿using Spotify.Models;
+
+namespace WebApp.Services;
+
+public sealed class Game
+{
+    private readonly Playlist _playlist;
+
+    public Game(Playlist playlist)
+    {
+        _playlist = playlist;
+    }
+
+
+}
